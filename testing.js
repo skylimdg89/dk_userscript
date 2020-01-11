c6542d34fdbefc0a2b839dae18e5e5d55242dce2
Dongkyu Lim <skylimdg89@gmail.com>
2020-01-11 10:20:38 +0900

    Create testing.js

---
+++ testing.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         testing
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        http://*/*
+// @updateURL
+// @downloadURL
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+})();
